f7b56376955141d09bfd3cf92510592255deff3b
Jimboy Cantila <jimcan009@gmail.com>
2020-11-18 20:09:18 +0800

    with appbar and signup page

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-    return (
-        <main>
-            <h1>Oh Yeah</h1>
-        </main>
-    );
-}
-
-export default App;
---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import ButtonAppBar from './Appbar';
+import SignUp from './Signin'
+
+function App() {
+    return (
+        <main>
+            <ButtonAppBar />
+            <SignUp />
+        </main>
+    );
+}
+
+export default App;
